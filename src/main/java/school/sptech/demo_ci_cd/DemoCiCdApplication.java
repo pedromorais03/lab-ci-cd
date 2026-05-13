ee0f07387b028967edb421feade681bd8478989b
pedromorais03 <pedromorais3012@gmail.com>
2026-05-12 21:10:19 -0300

    add spring project

---
+++ src/main/java/school/sptech/demo_ci_cd/DemoCiCdApplication.java
@@ -0,0 +1,13 @@
+package school.sptech.demo_ci_cd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCiCdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/school/sptech/demo_ci_cd/DemoCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package school.sptech.demo_ci_cd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
